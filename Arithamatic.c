fb3ee429eee711dab1c6be3a8e2b76b708a7ad34
pavanpawar4912 <82253373+pavanpawar4912@users.noreply.github.com>
2021-05-14 11:42:37 +0530

    Add files via upload

---
+++ Arithamatic.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    int a = 5;
+    int b =10;
+    int c = a+b;
+    int d = a-b;
+    float e = b/a;
+    int f = a*b;
+    printf("\n Addition Of a+b is %d",c);
+    printf("\n Substraction Of a-b is %d",d);
+    printf("\n Division Of b/a is %d",e);
+    printf("\n Addition Of a*b is %d",f);
+}
+
+
+
+
