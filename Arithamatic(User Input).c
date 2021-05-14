d7377444db96dfa041e5a99862d1ac6dd35dfbc2
pavanpawar4912 <82253373+pavanpawar4912@users.noreply.github.com>
2021-05-14 12:11:00 +0530

    Add files via upload

---
+++ Arithamatic(User Input).c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    printf("\n Enter The Value Of a");
+    scanf("%d",&a);
+    printf("\n Enter The Value Of b");
+    scanf("%d",&b);
+    printf("\n The Sum Of a and b %d",a+b);
+    printf(" \n The Sub Of a and b %d",a-b);
+    printf(" \n The div Of a and b %d",a/b);
+    printf(" \n The mult Of a and b %d",a*b);
+    return 0;
+}
