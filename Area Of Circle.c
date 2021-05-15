6ab9802a98bc3e88ce05a674d375e24b87c5d19e
pavanpawar4912 <82253373+pavanpawar4912@users.noreply.github.com>
2021-05-15 11:52:38 +0530

    Add files via upload

---
+++ Area Of Circle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int r;
+    float pi =3.14;
+    printf("\n Enter The Radius Of Circle");
+    scanf("%d",&r);
+    printf("Area Of Circle %f",3.14*r*r);
+    return 0;
+}
