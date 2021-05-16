508981766a4377fc67476f62769a151810a2e573
pavanpawar4912 <82253373+pavanpawar4912@users.noreply.github.com>
2021-05-16 11:41:59 +0530

    Add files via upload

---
+++ Basic program.c/Size Of Data Type.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+ void main()
+ {
+    printf("\n Size Of int :");
+    printf("%d\n\n",sizeof(int));
+
+    printf("\n Size Of float :");
+    printf(" %d\n\n",sizeof(float));
+
+    printf("\n Size Of char :");
+    printf(" %d\n\n",sizeof(char));
+
+    printf("\n Size Of double :");
+    printf(" %d\n\n",sizeof(double));
+
+    printf("\n Size Of long :");
+    printf(" %d\n\n",sizeof(long));
+
+    printf("\n Size Of unsigned int: ");
+    printf(" %d\n\n",sizeof(float));
+
+    printf("\n Size Of unsigned long: ");
+    printf(" %d\n\n",sizeof(float));
+
+    printf("size of long  int: ");
+    printf("%d\n\n",sizeof(long int));
+
+
+    return 0;
+
+ }
