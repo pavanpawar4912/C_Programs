8479fa569c053b4f1a6550decfbce750cadff481
pavanpawar4912 <82253373+pavanpawar4912@users.noreply.github.com>
2021-05-17 09:58:08 +0530

    Create EvenOdd.c

---
+++ Coditional Statement/EvenOdd.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main()
+{
+    int num ;
+     printf(" \n Enter The Number Which We Want check : ");
+     scanf("%d",&num);
+     if(num%2==0)
+     {
+         printf(" \n The Number Is Even");
+     }
+     else
+     {
+         printf("\n The Number Is Odd");
+     }
+     return 0;
+}
+
