ffc92896f437972c86b555cfe3e0cc6502ceca56
pavanpawar4912 <82253373+pavanpawar4912@users.noreply.github.com>
2021-05-19 14:07:10 +0530

    Add files via upload

---
+++ Coditional Statement/SwitchCase.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+void main()
+{
+
+    int rating;
+    printf("Enter You Rating\n");
+    scanf("%d",&rating);
+    switch(rating)
+    {
+    case 1:
+        printf("Your rating is 1\n");
+        break;
+    case 2:
+        printf("Your rating is 2\n");
+        break;
+    case 3:
+        printf("Your rating is 3\n");
+        break;
+    case 4:
+        printf("Your rating is 4\n");
+        break;
+    case 5:
+        printf("Your rating is 5\n");
+        break;
+    default:
+        printf("You Have Entered Invalid rating\n");
+        break;
+    }
+}
