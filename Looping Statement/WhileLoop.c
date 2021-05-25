e7539e0a4e8b4bdcd5d3cbd298d34d7121b4d974
pavanpawar4912 <82253373+pavanpawar4912@users.noreply.github.com>
2021-05-25 10:23:03 +0530

    Create WhileLoop.c

---
+++ Looping Statement/WhileLoop.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main()
+{
+    int a;
+    printf("Enter The Value of a\n");
+    scanf("%d",&a);
+    while(a<10)
+    {
+       printf("%d\n",a);
+       a++;
+    }
+    return 0;
+}
