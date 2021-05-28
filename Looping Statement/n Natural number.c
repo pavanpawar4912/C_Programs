794b97dbf205e7a425e0de9994edbaebd54ac8fc
pavanpawar4912 <82253373+pavanpawar4912@users.noreply.github.com>
2021-05-28 12:22:59 +0530

    Add files via upload

---
+++ Looping Statement/n Natural number.c
@@ -0,0 +1,17 @@
+
+
+#include<stdio.h>
+void main()
+{
+    int i =0;
+    int n;
+    printf("Enter a Number \n");
+    scanf("%d",&n);
+    do{
+        prinf("The Number Is %d\n",i);
+        i++;
+    }
+    while(i<n);
+    return 0;
+
+}
