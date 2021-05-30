b9d681b3d06deb3b36342a519b35f88a11d5ff7c
pavanpawar4912 <82253373+pavanpawar4912@users.noreply.github.com>
2021-05-30 14:10:13 +0530

    Add files via upload

---
+++ Looping Statement/n Natural number Decrement.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+
+    int i,n;
+    printf("Enter The Number \n");
+    scanf("%d",&n);
+    for( i=n ; i ; i--)
+    {
+        printf("The Number Is %d\n",i);
+    }
+    return 0;
+}
