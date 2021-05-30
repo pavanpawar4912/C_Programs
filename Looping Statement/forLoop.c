1c5c146dcb24ee2ae5b6dcdebece6ec39e69e86a
pavanpawar4912 <82253373+pavanpawar4912@users.noreply.github.com>
2021-05-30 13:51:49 +0530

    Add files via upload

---
+++ Looping Statement/forLoop.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int i;
+    int a;
+    printf("Enter The Value Of a\n");
+    scanf("%d",&a);
+    for(i=0; i<a; i++)
+    {
+        printf("The value Of a is %d\n",i);
+    }
+    return 0;
+}
