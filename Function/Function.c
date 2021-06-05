c7acdd2692a38e6325a1b11deb6088fb1c1e26cc
pavanpawar4912 <82253373+pavanpawar4912@users.noreply.github.com>
2021-06-05 20:02:39 +0530

    Create Function.c

---
+++ Function/Function.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void gm();
+void ga();
+void ge();
+
+ int main()
+ {
+    gm();
+    ga();
+    ge();
+    return 0;
+
+ }
+  void gm()
+  {
+      printf("Good Morning\n");
+  }
+  void ga()
+  {
+      printf("Good Afternoon\n");
+  }
+  void ge()
+  {
+      printf("Good Evening\n");
+  }
