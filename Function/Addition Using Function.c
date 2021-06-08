7c76cdbcccbed8b494ae86928a8e62361fb171bf
pavanpawar4912 <82253373+pavanpawar4912@users.noreply.github.com>
2021-06-08 10:36:52 +0530

    Add files via upload

---
+++ Function/Addition Using Function.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int sum(int a,int b);
+int main()
+{
+int c;
+c =sum(2,5);
+printf("The Vale Of C is %d \n",c);
+return 0;
+}
+int sum(int a,int b)
+{
+    int result;
+    result = a+b;
+    return result;
+}
+
+
