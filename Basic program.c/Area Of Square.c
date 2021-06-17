06b8232073935f9eed9f1865812258249b41f414
pavanpawar4912 <82253373+pavanpawar4912@users.noreply.github.com>
2021-06-17 11:37:11 +0530

    Add files via upload

---
+++ Basic program.c/Area Of Square.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+ int main()
+ {
+     int side;
+     printf("Enter the side of square\n");
+     scanf("%d",&side);
+     printf("Area of square is %f",pow(side,2));
+     return 0;
+ }
