fee03d8255a98f4d6468ffc2b72ba9dd0880790a
pavanpawar4912 <82253373+pavanpawar4912@users.noreply.github.com>
2021-07-01 14:20:00 +0530

    Add files via upload

---
+++ Pointer/Pointer Call By Value.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int  sum(int a,int b);
+int main()
+{
+    int a =4, b = 5;
+    printf("The Value of a and b is %d and %d\n",a,b);
+    printf("The Sum Is %d\n",sum(a,b));
+    return 0;
+}
+ int sum (int a,int b)
+ {
+     int c;
+     c = a+b;
+     b = 3434;
+     a =2514;
+     return c;
+ }
