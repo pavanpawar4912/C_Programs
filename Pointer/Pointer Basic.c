f7f59977deb123c4f78d0cbcfa681b3dccfcf3d8
pavanpawar4912 <82253373+pavanpawar4912@users.noreply.github.com>
2021-07-01 13:51:27 +0530

    Update Pointer Basic.c

--- Pointer/Pointer Basic.c
+++ Pointer/Pointer Basic.c
@@ -2,7 +2,7 @@
 int main()
 {
     int i =34;
-    int *j =&i;
+    int *j =&i;//J will store The Address of i
     printf("The Value Of i is %d\n",i);
     printf("The Value Of j is %d\n",*j);
     printf("The Address of i is %u\n",&i);
