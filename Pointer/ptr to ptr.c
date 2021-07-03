139a2c620ac46438cb3f8bd6c0c818eba5e057db
pavanpawar4912 <82253373+pavanpawar4912@users.noreply.github.com>
2021-07-03 12:35:17 +0530

    Add files via upload

---
+++ Pointer/ptr to ptr.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+ int main()
+ {
+     int i = 354;
+     int *ptr;
+     int **ptr_ptr;
+     ptr = &i;
+     ptr_ptr=&ptr;
+     printf("The value of i is %d\n",**ptr_ptr);
+     return 0;
+ }
