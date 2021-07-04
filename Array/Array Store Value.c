5518b39ceff2cb38b4e3e20523f0be140eb6633b
pavanpawar4912 <82253373+pavanpawar4912@users.noreply.github.com>
2021-07-04 12:40:28 +0530

    Add files via upload

---
+++ Array/Array Store Value.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int array[3] ={22,25,26};
+    printf("The Value Of Array[1] is %d\n",array[0]);
+    printf("The Value Of Array[2] is %d\n",array[1]);
+    printf("The Value Of Array[3] is %d\n",array[2]);
+    return 0;
+}
